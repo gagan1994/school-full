01019d91b880c82f79402ecd9a2f2852cc600c6e
gagan s patil <gagan.patil@tarams.com>
2020-09-01 15:09:56 +0530

    nav bar top

--- school-web-app/src/app/App.js
+++ school-web-app/src/app/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import SchoolNavbar from '../navbar/nav-bar';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  render() {
+    return (
+      <div className="App">
+        <SchoolNavbar/>
+        <div className="row full-col">
+          <div className='col-12  d-flex justify-content-center'>
+            <img src="https://d29u17ylf1ylz9.cloudfront.net/junior/images/icons/line-2.png"></img>
+            <img >
+            
+            </img>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
